3450e38fbb250537fc773311b3232f2e954cb826
TienYou-Nathan <44438062+TienYou-Nathan@users.noreply.github.com>
2022-05-23 09:18:20 +0200

    Update autoconnect.sh

--- autoconnect.sh
+++ autoconnect.sh
@@ -1,6 +1,12 @@
-url = $(curl -s -w "%{url_effective}\n" -L http://google.fr -o /dev/null)
-magic = echo "$url" | sed -E 's|.*\?(.*)$|\1|g'
-target = echo "$url" | sed -E 's|^([a-z]*://[a-z0-9\.]*(:[[:digit:]]*)?/).*|\1|g'
-redir = "https://google.fr"
+#!/bin/sh
+url=$(curl -s -w "%{url_effective}\n" -L http://google.fr -o /dev/null)
+magic=$(echo "$url" | sed -E 's|.*\?(.*)$|\1|g')
+target=$(echo "$url" | sed -E 's|^([a-z]*://[a-z0-9\.]*(:[[:digit:]]*)?/).*|\1|g')
+redir="https://google.fr"
+username=""
+password=""
 
-curl -X POST -d "magic=${magic}&4Tredir=${redir}&answer=1" 
+echo "$target"
+echo "$magic"
+curl -X POST -d "magic=${magic}&4Tredir=${redir}&answer=1" "$target"
+curl -X POST -w "%{url_effective}\n" -d "username=${username}&4Tredir=${redir}&password=${password}&magic=${magic}" "$target"
